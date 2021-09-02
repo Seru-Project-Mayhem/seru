3375dd99e5399ce3f56384fad9e0a070c9f486a7
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-09-02 16:46:31 -0500

    US-1

---
+++ src/main/resources/static/js/views/Home.js
@@ -0,0 +1,17 @@
+export default function Home(props) {
+    console.log("Get er done")
+    return `
+        <header>
+            <h1>Seru</h1>
+        </header>
+        <main>
+            <div>
+                <p>
+                    This is where all the cards and game images are going.
+                </p>    
+            </div>
+        </main>
+    `;
+
+
+}
