c955b2fc183e20e81963f38442e8b63dfec6d915
Amado Azua III <amado.azua3@gmail.com>
2021-09-12 17:50:49 -0500

    Refactored fetch queries. Deleted views for 0-10, 10-20, 20-30. Added infinite scrolling from Cheapshark API. Price slider that let's Users pick the max price of games they want to see.

--- src/main/resources/static/js/FetchQueries.js
+++ src/main/resources/static/js/FetchQueries.js
@@ -1,30 +1,33 @@
 
-import {cheapSharkCardBuilder, freeCardBuilder, ratingEvent, SetFavoriteEvent} from "./views/Home.js";
+import {
+	cheapSharkCardBuilder,
+	priceSliderEvent,
+	ratingEvent,
+	SetFavoriteEvent,
+} from "./views/Home.js";
 import {rapidApi_token} from "./ApiKeys/keys.js";
 import {apiData} from "./views/FreeToPlay.js";
-import createView from "./createView.js";
 
 
-export function freeToGameHomeGet(){
-	fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
-			"x-rapidapi-key": rapidApi_token,
-		}
-	})
-		.then(response => response.json())
-		.then(data => {
-			console.log("Adding free cards to DOM")
-			const size = 5;
-			const items = data.slice(0, size);
-			$("#container-free").append(freeCardBuilder(items));
-			cheapSharkZeroToTenHomeGet();
-		})
-		.catch(err => {
-			console.error(err);
-		});
-}
+// export function freeToGameHomeGet(){
+// 	fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", {
+// 		"method": "GET",
+// 		"headers": {
+// 			"x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+// 			"x-rapidapi-key": rapidApi_token,
+// 		}
+// 	})
+// 		.then(response => response.json())
+// 		.then(data => {
+// 			console.log("Adding free cards to DOM")
+// 			const size = 5;
+// 			const items = data.slice(0, size);
+// 			$("#container-free").append(freeCardBuilder(items));
+// 		})
+// 		.catch(err => {
+// 			console.error(err);
+// 		});
+// }
 
 export function freeToGameGet(){
 	fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", {
@@ -37,17 +40,14 @@ export function freeToGameGet(){
 		.then(response => response.json())
 		.then(data => {
 			apiData(data)
-			// const size = 25;
-			// const items = data.slice(0, size);
-			// $("#container-free-to-play-page").append(freeCardBuilder(items));
 		})
 		.catch(err => {
 			console.error(err);
 		});
 }
 
-export function cheapSharkZeroToTenHomeGet(){
-	fetch("https://cheapshark-game-deals.p.rapidapi.com/deals?lowerPrice=0&steamRating=0&desc=true&output=json&steamworks=false&sortBy=Savings&AAA=0&pageSize=5&exact=0&upperPrice=10&pageNumber=0&onSale=0&metacritic=0", {
+export function cheapSharkGet(){
+	fetch("https://cheapshark-game-deals.p.rapidapi.com/deals?lowerPrice=0&steamRating=0&desc=true&output=json&steamworks=false&sortBy=Savings&AAA=0&pageSize=60&exact=0&upperPrice=60&pageNumber=0&onSale=0&metacritic=0", {
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
@@ -56,99 +56,10 @@ export function cheapSharkZeroToTenHomeGet(){
 	})
 		.then(response => response.json())
 		.then(data => {
-			$("#container-zerototen").append(cheapSharkCardBuilder(data));
-			cheapSharkTenToTwentyHomeGet();
-			console.log("Cheapshark card builder appended");
-		})
-		.catch(err => {
-			console.error(err);
-		});
-}
-
-export function cheapSharkZeroToTenGet(){
-	fetch("https://cheapshark-game-deals.p.rapidapi.com/deals?lowerPrice=0&steamRating=0&desc=true&output=json&steamworks=false&sortBy=Savings&AAA=0&pageSize=25&exact=0&upperPrice=10&pageNumber=0&onSale=0&metacritic=0", {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
-			"x-rapidapi-key": rapidApi_token,
-		}
-	})
-		.then(response => response.json())
-		.then(data => {
-			$("#container-zero-to-ten-page").append(cheapSharkCardBuilder(data));
-		})
-		.catch(err => {
-			console.error(err);
-		});
-}
-
-export function cheapSharkTenToTwentyHomeGet(){
-	fetch("https://cheapshark-game-deals.p.rapidapi.com/deals?lowerPrice=10&steamRating=0&desc=true&output=json&steamworks=false&sortBy=Savings&AAA=0&pageSize=5&exact=0&upperPrice=20&pageNumber=0&onSale=0&metacritic=0", {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
-			"x-rapidapi-key": rapidApi_token,
-		}
-	})
-		.then(response => response.json())
-		.then(data => {
-			$("#container-tentotwenty").append(cheapSharkCardBuilder(data));
-			cheapSharkTwentyToThirtyHomeGet();
-			console.log("Cheapshark ten to twenty appended");
-		})
-		.catch(err => {
-			console.error(err);
-		});
-}
-
-export function cheapSharkTenToTwentyGet(){
-	fetch("https://cheapshark-game-deals.p.rapidapi.com/deals?lowerPrice=10&steamRating=0&desc=true&output=json&steamworks=false&sortBy=Savings&AAA=0&pageSize=25&exact=0&upperPrice=20&pageNumber=0&onSale=0&metacritic=0", {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
-			"x-rapidapi-key": rapidApi_token,
-		}
-	})
-		.then(response => response.json())
-		.then(data => {
-			$("#container-ten-to-twenty-page").append(cheapSharkCardBuilder(data));
-		})
-		.catch(err => {
-			console.error(err);
-		});
-}
-
-
-export function cheapSharkTwentyToThirtyHomeGet(){
-	fetch("https://cheapshark-game-deals.p.rapidapi.com/deals?lowerPrice=20&steamRating=0&desc=true&output=json&steamworks=false&sortBy=Savings&AAA=0&pageSize=5&exact=0&upperPrice=30&pageNumber=0&onSale=0&metacritic=0", {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
-			"x-rapidapi-key": rapidApi_token,
-		}
-	})
-		.then(response => response.json())
-		.then(data => {
-			$("#container-twentytothirty").append(cheapSharkCardBuilder(data));
+			$("#container-games").append(cheapSharkCardBuilder(data));
 			SetFavoriteEvent();
 			ratingEvent();
-		})
-		.catch(err => {
-			console.error(err);
-		});
-}
-
-export function cheapSharkTwentyToThirtyGet(){
-	fetch("https://cheapshark-game-deals.p.rapidapi.com/deals?lowerPrice=20&steamRating=0&desc=true&output=json&steamworks=false&sortBy=Savings&AAA=0&pageSize=25&exact=0&upperPrice=30&pageNumber=0&onSale=0&metacritic=0", {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
-			"x-rapidapi-key": rapidApi_token,
-		}
-	})
-		.then(response => response.json())
-		.then(data => {
-			$("#container-twenty-to-thirty-page").append(cheapSharkCardBuilder(data));
+			priceSliderEvent();
 		})
 		.catch(err => {
 			console.error(err);
@@ -182,27 +93,6 @@ export function postRating(rating){
 	});
 }
 
-// STOP!!! DON'T DELETE THIS SECTION WE WILL NEED IT FOR THE DATABASE
-
-// export function cheapSharkGetAll(){
-//
-// 	fetch("https://cheapshark-game-deals.p.rapidapi.com/deals?lowerPrice=0&steamRating=0&desc=0&output=json&steamworks=0&sortBy=store&AAA=0&pageSize=60&exact=0&upperPrice=50&pageNumber=10&onSale=false&metacritic=0&storeID=1%2C2%2C3%2C7%2C11%2C15%2C16%2C23%2C24%2C25%2C29%2C30%2C31", {
-// 		"method": "GET",
-// 		"headers": {
-// 			"x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
-// 			"x-rapidapi-key": rapidApi_token,
-// 		}
-// 	})
-// 		.then(response => response.json())
-// 		.then(data => {
-// 			console.log(data);
-// 		})
-// 		.catch(err => {
-// 			console.error(err);
-// 		});
-// }
-
-
 export function getAllGames(){
-	freeToGameHomeGet();
+	cheapSharkGet();
 }
--- src/main/resources/static/js/views/FreeToPlay.js
+++ src/main/resources/static/js/views/FreeToPlay.js
@@ -1,4 +1,3 @@
-import {freeCardBuilder} from "./Home.js";
 
 export default function FreeToPlayView(){
 
@@ -15,6 +14,61 @@ export default function FreeToPlayView(){
     <footer style="margin-top: 5em"></footer>
     `;
 
+}
+
+export function freeCardBuilder(listOfGames) {
+
+    return listOfGames.map(game => {
+        return `
+<div class="flip-card" style="width: 18rem;">
+  <div class="flip-card-inner">
+  	<div class="flip-card-front">
+    	<img class="card-img-top" src="${game.thumbnail}" alt="Card image cap">
+  		<div class="card-body">
+    		<h5 class="card-title" > ${game.title}</h5>
+    		<p class="card-text" > Free to play </p>
+       
+    	</div>
+    </div>
+    <div class="flip-card-back">
+      <h5 class="card-title" > ${game.title}</h5>
+     	<p class="card-text" > ${game.short_description} </p>
+    	<p class="card-text" > Free to play </p>
+    	<button class="review-btn">Leave a Review</button>
+    	<form>
+    	    <input class="editRating" type="number" min="0" max="10" step="0.5">
+    	    <button class="submitForm">Submit</button>
+        </form>
+        <div class="var2">
+    <a class="button two inactive desktop">
+        <div class="icon-with-text">
+            <div class="icon-with-text__icon">
+                <div class="btn__effect">
+                    <svg class="heart-stroke icon-svg icon-svg--size-4 icon-svg--color-silver" viewBox="20 18 29 28" aria-hidden="true" focusable="false"><path d="M28.3 21.1a4.3 4.3 0 0 1 4.1 2.6 2.5 2.5 0 0 0 2.3 1.7c1 0 1.7-.6 2.2-1.7a3.7 3.7 0 0 1 3.7-2.6c2.7 0 5.2 2.7 5.3 5.8.2 4-5.4 11.2-9.3 15a2.8 2.8 0 0 1-2 1 3.4 3.4 0 0 1-2.2-1c-9.6-10-9.4-13.2-9.3-15 0-1 .6-5.8 5.2-5.8m0-3c-5.3 0-7.9 4.3-8.2 8.5-.2 3.2.4 7.2 10.2 17.4a6.3 6.3 0 0 0 4.3 1.9 5.7 5.7 0 0 0 4.1-1.9c1.1-1 10.6-10.7 10.3-17.3-.2-4.6-4-8.6-8.4-8.6a7.6 7.6 0 0 0-6 2.7 8.1 8.1 0 0 0-6.2-2.7z"></path>
+                    </svg>
+                    <svg class="heart-full icon-svg icon-svg--size-4 icon-svg--color-blue" viewBox="0 0 19.2 18.5" aria-hidden="true" focusable="false"><path d="M9.66 18.48a4.23 4.23 0 0 1-2.89-1.22C.29 10.44-.12 7.79.02 5.67.21 2.87 1.95.03 5.42.01c1.61-.07 3.16.57 4.25 1.76A5.07 5.07 0 0 1 13.6 0c2.88 0 5.43 2.66 5.59 5.74.2 4.37-6.09 10.79-6.8 11.5-.71.77-1.7 1.21-2.74 1.23z"></path></svg>
+                    <svg class="broken-heart" xmlns="http://www.w3.org/2000/svg" width="48" height="16" viewBox="5.707 17 48 16"><g fill="#0090E3">
+                        <path class="broken-heart--left" d="M29.865 32.735V18.703a4.562 4.562 0 0 0-3.567-1.476c-2.916.017-4.378 2.403-4.538 4.756-.118 1.781.227 4.006 5.672 9.737a3.544 3.544 0 0 0 2.428 1.025l-.008-.008.013-.002z"/>
+                        <path class="broken-heart--right" d="M37.868 22.045c-.135-2.588-2.277-4.823-4.697-4.823a4.258 4.258 0 0 0-3.302 1.487l-.004-.003v14.035a3.215 3.215 0 0 0 2.289-1.033c.598-.596 5.882-5.99 5.714-9.663z"/></g>
+                        <path class="broken-heart--crack" fill="none" stroke="#FFF" stroke-miterlimit="10" d="M29.865 18.205v14.573"/></svg>
+                    <div class="effect-group">
+                        <span class="effect"></span>
+                        <span class="effect"></span>
+                        <span class="effect"></span>
+                        <span class="effect"></span>
+                        <span class="effect"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+</div>
+            		<button type="button" class="btn-get btn btn-primary" onclick=" window.open('${game.game_url}','_blank')">Get Now</button>
+
+    </div>
+  </div>
+</div>`
+    });
 }
 
 export function apiData(data){
