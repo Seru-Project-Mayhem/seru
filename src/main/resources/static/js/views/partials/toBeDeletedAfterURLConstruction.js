48c0c0686dd69febaf387d694e3b8dd19ecf50f0
Sledgerobby35 <sledge.robby35@gmail.com>
2021-09-13 13:55:04 -0500

    added CheapShark url builder

---
+++ src/main/resources/static/js/views/partials/toBeDeletedAfterURLConstruction.js
@@ -0,0 +1,38 @@
+Steam -  https://steampowered.com/SteamAppId/Name_Of_Game
+storeID: 1
+
+GamersGate - https://www.gamersgate.com/product/Name-Of-Game/
+storeID: 2
+
+GreenManGaming - https://www.greenmangaming.com/games/Name-Of-Game
+storeID: 3
+
+GOG - https://www.gog.com/game/Name_Of_Game
+storeID: 7
+
+Humble Store -  https://www.humblebundle.com/store/name-of-game
+storeID: 11
+
+Fanatical - https://www.fanatical.com/en/game/name-of-game
+storeID: 15
+
+Gamesrocket - https://www.gamesrocket.com/name-of-game.html
+storeID: 16
+
+GameBillet - https://www.gamebillet.com/name-of-game
+storeID: 23
+
+Voidu - https://www.voidu.com/en/name-of-game
+storeID: 24
+
+Epic Games Store - https://www.epicgames.com/store/en-US/p/name-of-game
+storeID: 25
+
+2Game - https://2game.com/en-us/name-of-game
+storeID: 29
+
+IndieGala - https://www.indiegala.com/store/game/name-of-game/SteamAppId
+storeID: 30
+
+Blizzard Shop - https://us.shop.battle.net/en-us/family/name-of-game
+storeID: 31
