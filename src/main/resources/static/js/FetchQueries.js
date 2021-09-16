44f0e7ab77dd4f922b7ed34c8684e94ea5fca2fb
Amado Azua III <amado.azua3@gmail.com>
2021-09-16 15:58:01 -0500

    All 13 stores now have .png or .svg to render on the card, fix queries to call only the 13 stores we are linking to

--- src/main/resources/static/js/FetchQueries.js
+++ src/main/resources/static/js/FetchQueries.js
@@ -44,7 +44,7 @@ export function freeToGameGet(){
 }
 
 export function cheapSharkGet(){
-	fetch("https://cheapshark-game-deals.p.rapidapi.com/deals?lowerPrice=0&steamRating=0&desc=true&output=json&steamworks=false&sortBy=Savings&AAA=0&pageSize=60&exact=0&upperPrice=60&pageNumber=0&onSale=0&metacritic=0", {
+	fetch("https://cheapshark-game-deals.p.rapidapi.com/deals?storeID=1%2C2%2C3%2C7%2C11%2C15%2C16%2C23%2C24%2C25%2C29%2C30%2C31&metacritic=0&onSale=0&pageNumber=0&upperPrice=50&exact=0&pageSize=60&AAA=0&sortBy=Savings&steamworks=0&output=json&desc=0&steamRating=0&lowerPrice=0", {
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
