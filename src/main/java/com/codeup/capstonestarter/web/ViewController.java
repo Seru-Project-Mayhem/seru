38b31036b4571b695a0e2d387db2133e58d72593
Amado Azua III <amado.azua3@gmail.com>
2021-09-20 15:51:00 -0500

    fixing nav to /browse

--- src/main/java/com/codeup/capstonestarter/web/ViewController.java
+++ src/main/java/com/codeup/capstonestarter/web/ViewController.java
@@ -2,10 +2,12 @@ package com.codeup.capstonestarter.web;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@CrossOrigin
 public class ViewController {
 
     @Value("${rapidApi_token}")
