231eebecde4c993dc338442fe9e11b1947a06912
Amado Azua III <amado.azua3@gmail.com>
2021-09-20 16:30:59 -0500

    fixed booting application locally and accessing api_token

--- src/main/java/com/codeup/capstonestarter/web/ViewController.java
+++ src/main/java/com/codeup/capstonestarter/web/ViewController.java
@@ -2,7 +2,6 @@ package com.codeup.capstonestarter.web;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
